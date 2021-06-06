0e901ac2a0001a0809fb032b6f50fa053f38007c
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-06 13:46:56 +0700

    Add cp to debug sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 set -e
 
+VERSION=$1
+
 sed -i '' "s+# PODS_BINARY_MARK+require_relative 'scripts/prebuild_debug.rb'\nprebuild_debug!+g" Podfile
 
 # SCRIPTS_DIR="$(dirname "$0")"
@@ -11,4 +13,13 @@ sed -i '' 's+:path+:binary => true, :git+g' Podfile
 pod install
 pod binary prebuild
 
+cp -f -R _Prebuild/**/*.xcframework submodules/sdk/Frameworks/
+echo "✅ Copy Frameworks to BomBiSDK"
+
+if [ ! -z $VERSION ]; then
+    cd submodules/sdk
+    sh scripts/publish.sh "$VERSION"
+fi
+
+exit 0
 # git reset --hard
