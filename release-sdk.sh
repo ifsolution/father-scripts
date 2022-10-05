9ee198292420bf7f6ce4d83732ffc59d0b19c0f9
NGUYEN CHI CONG <congnc.if@gmail.com>
2022-10-05 13:24:18 +0700

    Add release-sdk.sh

---
+++ release-sdk.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+set -e
+
+VERSION=$(cat sdk-version)
+echo "Fixed version $VERSION"
+
+git fetch --tags --recurse-submodules=no
+
+TAG=$(git describe --tags --abbrev=0)
+echo "Tag version $TAG"
+
+# envman add --key LATEST_SDK_VERSION --value ${VERSION}
+
+if [ "$VERSION" == "$TAG" ]; then
+    echo "Latest SDK version $TAG. Skip releasing a new version."
+    # envman add --key BUILD_MESSAGE --value "Latest SDK version $TAG. Skip releasing a new version."
+    exit 0
+else
+    echo "Prepare to release SDK version $VERSION"
+
+    # publish new version
+    pod repo bump $VERSION
+
+    git submodule foreach git tag $version
+    git submodule foreach git push origin --tags
+
+    pod repo sync father
+
+    git submodule foreach git reset --hard
+
+    # push new tag
+    git tag $VERSION
+    git push origin --tags
+
+    echo "Release SDK version $VERSION successfully"
+    # envman add --key BUILD_MESSAGE --value "Release SDK version $VERSION successfully"
+fi
